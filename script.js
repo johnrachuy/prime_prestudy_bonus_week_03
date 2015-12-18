f8bc72df3cb72bc61f6208c0eab60e4490423d4a
John Rachuy II <john.rachuy@gmail.com>
2015-12-17 19:16:45 -0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log(timesFive(minusThree(plusSix(4))));
+
+function timesFive (c) {
+   return c * 5;
+}
+
+function minusThree (b) {
+   return b - 3;
+}
+
+function plusSix(a) {
+   return a + 6;
+}
